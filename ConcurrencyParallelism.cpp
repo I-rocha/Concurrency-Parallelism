f4645f99902c51f3603190abe49ff72b87ee90d8
Israel Rocha <israel.rexc@yahoo.com>
2021-11-02 18:50:07 -0300

    script principal preparado

--- ConcurrencyParallelism.cpp
+++ ConcurrencyParallelism.cpp
@@ -8,22 +8,49 @@
 
 //	Personal	//
 #include "map.h"
+#include "mapCon.h"
 
 int main()
 {
 	Map* obj = new Map();
 	int** newGrid;
-
-	omp_set_num_threads(N_TRHEADS);
+	int i = 1;
 
 	obj->setFirstGrid();
 	std::cout << "Condicao inicial: " << obj->NSociety() << std::endl;
 
-	for (int i = 0; i < N_GEN; i++) {
+	for (i = 1; i <= N_GEN; i++) {
 		newGrid = obj->nextGen(HIGHLIFE);
 		obj->setGrid(newGrid);
-		std::cout << "Geracao " << i+1 << ": " << obj->NSociety() << std::endl;
+		
+
+		if (i <= 5) {
+			std::cout << "Geracao " << i << ": " << obj->NSociety() << std::endl;
+			obj->showState(0, 50);
+		}
 	}
-	obj->showState(0, 50);
+	std::cout << "Geracao " << i-1 << ": " << obj->NSociety() << std::endl;
+	
+
+	//MapCon* obj = new MapCon();
+	//int** newGrid;
+	//int i = 1;
+
+	//omp_set_num_threads(N_TRHEADS);
+
+	//obj->setFirstGrid();
+	//std::cout << "Condicao inicial: " << obj->NSociety() << std::endl;
+	//
+	//for (i = 1; i <= N_GEN; i++) {
+	//	newGrid = obj->nextGen(JOGO_DA_VIDA);
+	//	obj->setGrid(newGrid);
+
+	//	if (i <= 5) {
+	//		std::cout << "Geracao " << i << ": " << obj->NSociety() << std::endl;
+	//		obj->showState(0, 50);
+	//	}
+	//}
+
+	//std::cout << "Geracao " << i - 1 << ": " << obj->NSociety() << std::endl;
 	
 }
