f19bfefdf752f9282c82ea0c5a12181d3d59b10c
Israel Rocha <israel.rexc@yahoo.com>
2021-11-02 15:36:29 -0300

    Adicionar arquivos de projeto.

---
+++ ConcurrencyParallelism.cpp
@@ -0,0 +1,27 @@
+// Concurrency and Parallelism
+// Israel da Rocha
+//
+
+// C++	//
+#include <iostream>
+#include <omp.h>
+
+//	Personal	//
+#include "map.h"
+
+int main()
+{
+	Map* obj = new Map();
+	int** newGrid;
+
+	obj->setFirstGrid();
+	std::cout << "Condicao inicial: " << obj->NSociety() << std::endl;
+
+	for (int i = 0; i < N_GEN; i++) {
+		newGrid = obj->nextGen(HIGHLIFE);
+		obj->setGrid(newGrid);
+		std::cout << "Geracao " << i+1 << ": " << obj->NSociety() << std::endl;
+	}
+	obj->showState(0, 50);
+	
+}
